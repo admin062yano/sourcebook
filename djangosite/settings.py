9cd12f8eb7d9d336b9f918f4c7b9116c97a172db
admin062yano <superadm.062@gmail.com>
2019-08-31 01:37:08 +0900

    test 7

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = "staticfiles"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 LOGIN_REDIRECT_URL = 'sourcebook:Index'
 LOGOUT_REDIRECT_URL = 'sourcebook:Index'
 
