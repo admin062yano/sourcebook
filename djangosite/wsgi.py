550ca8adcc4d2894ed35d39e66565a87e3bfae76
admin062yano <superadm.062@gmail.com>
2019-08-31 02:13:05 +0900

    test 10

--- djangosite/wsgi.py
+++ djangosite/wsgi.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+#from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangosite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
